f44f6418e53424bd850bf4bd0b82ae90cefeff09
Ian Moffett <ian@osmora.org>
2025-10-24 05:51:17 -0400

    sdk: Add defs.h
    Signed-off-by: Ian Moffett <ian@osmora.org>

---
+++ sdk/include/ke/defs.h
@@ -0,0 +1,17 @@
+/*
+ * ALGAE Kernel C definitions
+ * Copyright (c) 2025, Ian Moffett and the Algae team.
+ * All rights reserved.
+ * Provided under the BSD 3-Clause license.
+ */
+
+#ifndef _KE_DEFS_H_
+#define _KE_DEFS_H_ 1
+
+/* Attribute macros */
+#define PACKED  __attribute__((__packed__))
+
+/* Bitwise helpers */
+#define BIT(n) (1ULL << (n))
+
+#endif  /* !_KE_DEFS_H_ */
