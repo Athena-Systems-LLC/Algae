0d402b5b734323f49f8d6c57a8137a99ec9b93f8
Ian Moffett <ian@osmora.org>
2025-10-24 14:16:03 -0400

    ntos: build: Add default MI/MD CFLAGS
    Signed-off-by: Ian Moffett <ian@osmora.org>

---
+++ ntos/data/sys.mk
@@ -0,0 +1,13 @@
+# TODO: Make less rigid
+override MD_CFLAGS = -mno-80387 -mno-mmx -mno-3dnow \
+	-mno-sse -mno-sse2 -mno-red-zone -mcmodel=kernel
+
+override MI_CFLAGS = -fexceptions --std=gnu11 -ffreestanding -fno-stack-protector -fno-pic \
+	-Werror=implicit -Werror=implicit-function-declaration \
+	-Werror=implicit-int -Werror=int-conversion \
+    -Werror=missing-prototypes  -Isys/include/lib/ \
+	-Werror=incompatible-pointer-types -Werror=int-to-pointer-cast \
+	-Werror=return-type -mno-red-zone -mcmodel=kernel \
+	-D_KERNEL -Wno-pointer-sign -MMD -nostdinc \
+    -Wno-format-pedantic -Wno-attributes -D_KERNEL $(MD_CFLAGS) -O0 \
+	-Wno-switch -Wno-c23-extensions
