e81faa11d66927fdedbce80b23a5d579aa53b699
Ian Moffett <ian@osmora.org>
2025-10-24 06:11:00 -0400

    sdk: ke: Add extra macros to defs.h
    Signed-off-by: Ian Moffett <ian@osmora.org>

--- sdk/include/ke/defs.h
+++ sdk/include/ke/defs.h
@@ -9,9 +9,13 @@
 #define _KE_DEFS_H_ 1
 
 /* Attribute macros */
-#define PACKED  __attribute__((__packed__))
+#define PACKED          __attribute__((__packed__))
+#define ALWAYS_INLINE   __attribute__((__always_inline__))
 
 /* Bitwise helpers */
 #define BIT(n) (1ULL << (n))
+
+/* Misc macros */
+#define ASMV __asm__ __volatile__
 
 #endif  /* !_KE_DEFS_H_ */
