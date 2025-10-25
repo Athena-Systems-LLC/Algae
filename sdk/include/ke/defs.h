a8d0c003ee2c9ce225cce12026874c027f8cec3b
Ian Moffett <ian@osmora.org>
2025-10-24 23:34:19 -0400

    sdk: ke: Add alignment helper macros to defs.h
    Signed-off-by: Ian Moffett <ian@osmora.org>

--- sdk/include/ke/defs.h
+++ sdk/include/ke/defs.h
@@ -39,6 +39,10 @@
 #define PTR_OFFSET(PTR, OFF) ((void *)((ULONG_PTR)PTR + OFF))
 #define PTR_NOFFSET(PTR, NOFF) ((void *)((ULONG_PTR)PTR - NOFF))
 
+/* Align up/down a value */
+#define ALIGN_DOWN(value, align)      ((value) & ~((align)-1))
+#define ALIGN_UP(value, align)        (((value) + (align)-1) & ~((align)-1))
+
 /* Number of elements in an array */
 #define NELEM(a) (sizeof(a) / sizeof(a[0]))
 
