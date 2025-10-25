353474ad85e02028c6ec3af6276654343c2d9a56
Ian Moffett <ian@osmora.org>
2025-10-24 22:03:36 -0400

    ntos/amd64: trap: Bug check on CPU faults
    Signed-off-by: Ian Moffett <ian@osmora.org>

--- ntos/platform/amd64/cpu/trap.c
+++ ntos/platform/amd64/cpu/trap.c
@@ -6,9 +6,34 @@
  */
 
 #include <md/trap.h>
+#include <ke/defs.h>
+#include <ke/bugCheck.h>
+
+static const char *trapTab[] = {
+    [TRAP_NONE]         = "bad",
+    [TRAP_BREAKPOINT]   = "breakpoint",
+    [TRAP_ARITH_ERR]    = "arithmetic error",
+    [TRAP_OVERFLOW]     = "overflow",
+    [TRAP_BOUND_RANGE]  = "bound range exceeded",
+    [TRAP_INVLOP]       = "invalid opcode",
+    [TRAP_DOUBLE_FAULT] = "double fault",
+    [TRAP_INVLTSS]      = "invalid TSS",
+    [TRAP_SEGNP]        = "segment not present",
+    [TRAP_PROTFLT]      = "general protection",
+    [TRAP_PAGEFLT]      = "page fault",
+    [TRAP_NMI]          = "non-maskable interrupt",
+    [TRAP_SS]           = "stack-segment fault"
+};
 
 void
 trap_handler(struct trapFrame *tf)
 {
-    for (;;);
+    const char *trapStr;
+
+    if (tf->trapno > NELEM(trapTab)) {
+        keBugCheck("bad trap type %x\n", tf->trapno);
+    }
+
+    trapStr = trapTab[tf->trapno];
+    keBugCheck("got fatal trap: %s\n", trapStr);
 }
