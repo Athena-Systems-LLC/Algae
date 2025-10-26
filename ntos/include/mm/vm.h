d50654cf3667e27da372298dd95fad5f37f40582
Ian Moffett <ian@osmora.org>
2025-10-25 23:55:21 -0400

    ntos: mm: Add virtual memory page allocation
    Introduces new mmAllocFrame() function for allocating virtual memory
    pages
    
    Signed-off-by: Ian Moffett <ian@osmora.org>

--- ntos/include/mm/vm.h
+++ ntos/include/mm/vm.h
@@ -9,9 +9,21 @@
 #define _MM_VM_H_ 1
 
 #include <ke/bootParam.h>
+#include <ke/types.h>
 
 #define PAGESIZE 4096
 #define PHYS_TO_VIRT(PA) (void *)((PA) + keGetKernelBase())
 #define VIRT_TO_PHYS(VIRT) (((ULONG_PTR)(VIRT)) - keGetKernelBase())
+
+/*
+ * Allocate virtual memory pages
+ *
+ * @count: Number of pages to allocate
+ * @flags: Optional flags
+ *
+ * Returns the base of the contiguous memory region
+ * on success, otherwise NULL on failure
+ */
+void *mmAllocPage(USIZE count, USHORT flags);
 
 #endif  /* !_MM_VM_H_ */
---
+++ ntos/mm/page.c
@@ -0,0 +1,22 @@
+/*
+ * ALGAE virtual memory page management
+ * Copyright (c) 2025, Ian Moffett and the Algae team.
+ * All rights reserved.
+ * Provided under the BSD 3-Clause license.
+ */
+
+#include <mm/vm.h>
+#include <mm/phys.h>
+
+void *
+mmAllocPage(USIZE count, USHORT flags)
+{
+    ULONG_PTR pBase;
+
+    pBase = mmAllocFrame(count);
+    if (pBase == 0) {
+        return NULL;
+    }
+
+    return PHYS_TO_VIRT(pBase);
+}
