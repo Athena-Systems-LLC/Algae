bd35bbf1f113877380cb707a8a5f33fdf0f2ca59
Ian Moffett <ian@osmora.org>
2025-10-25 20:08:24 -0400

    ntos: ke: Rework bug check internals
    - Add heading and body
    - Add bug check logging helper
    - Center text
    
    Signed-off-by: Ian Moffett <ian@osmora.org>

--- ntos/ke/bugCheck.c
+++ ntos/ke/bugCheck.c
@@ -7,6 +7,7 @@
 
 #include <ke/bugCheck.h>
 #include <ke/vaargs.h>
+#include <ke/bootParam.h>
 #include <rtl/string.h>
 #include <drv/bootVid.h>
 #include <hal/cpu.h>
@@ -14,32 +15,110 @@
 #define BUG_CHECK_BG 0x021AE1
 #define BUG_CHECK_FG 0xFFFFFF
 
+/* Heading parameters */
+#define HEADING_FG BUG_CHECK_BG
+#define HEADING_BG 0xAAAAAA
+
+/* Body parameters */
+#define BODY_FG BUG_CHECK_FG
+#define BODY_BG BUG_CHECK_BG
+
+/* Diagnosis parameters */
+#define DIAG_FG BODY_FG
+#define DIAG_BG BODY_BG
+
+static struct bootParams params;
+static struct fbParams *fbParams;
+static ULONG curLogLine = 4;
+
+static void
+bugCheckPrint(ULONG fg, ULONG bg, const char *fmt, ...)
+{
+    va_list ap;
+    static char msg[256];
+
+    va_start(ap, fmt);
+    rtlBufPrintfV(
+        msg,
+        sizeof(msg),
+        fmt,
+        ap
+    );
+
+#define CENTER_XPOS(STR) \
+    ((fbParams->width) / 2) - (rtlStrlen((STR)) * FONT_WIDTH) / 2
+
+    /* Print the heading */
+    bootVidPrint(
+        CENTER_XPOS(msg),
+        curLogLine * FONT_HEIGHT,
+        fg,
+        bg,
+        msg
+    );
+#undef CENTER_XPOS
+    ++curLogLine;
+    va_end(ap);
+}
+
+static void
+bugCheckDump(const char *fmt, va_list diag_ap)
+{
+    static va_list ap;
+    static char fmtbuf[256];
+    static char heading[32];
+    static char msg[256];
+
+    /* Print the heading */
+    bugCheckPrint(
+        HEADING_FG,
+        HEADING_BG,
+        "ALGAE BUGCHECK"
+    );
+
+    /* Now the body */
+    ++curLogLine;
+    bugCheckPrint(
+        BODY_FG,
+        BODY_BG,
+        "A fatal error has occured and the system has been halted "
+        "to prevent damage to the machine"
+    );
+
+    /* Some bug reporting information */
+    bugCheckPrint(
+        BODY_FG,
+        BODY_BG,
+        "Please report this bug to ian@osmora.org"
+    );
+
+    /* Prep the actual error message */
+    rtlBufPrintfV(
+        fmtbuf,
+        sizeof(fmtbuf),
+        fmt,
+        diag_ap
+    );
+
+    ++curLogLine;
+    bugCheckPrint(
+        DIAG_FG,
+        DIAG_BG,
+        fmtbuf
+    );
+}
+
 NORETURN
 void keBugCheck(const char *fmt, ...)
 {
     va_list ap;
-    static char buf[512];
 
     va_start(ap, fmt);
+    keGetBootParams(&params, 0);
+    fbParams = &params.fbParams;
+
     bootVidClear(BUG_CHECK_BG);
-
-    rtlBufPrintfV(buf, sizeof(buf), fmt, ap);
-    bootVidPrint(
-        0,
-        0,
-        BUG_CHECK_FG,
-        BUG_CHECK_BG,
-        "** BUG CHECK **"
-    );
-
-    /* Print a line after */
-    bootVidPrint(
-        0,
-        FONT_HEIGHT,
-        BUG_CHECK_FG,
-        BUG_CHECK_BG,
-        buf
-    );
+    bugCheckDump(fmt, ap);
 
     halCpuHalt(CPU_HALT_SELF);
     __builtin_unreachable();
