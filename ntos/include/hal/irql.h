2bd6de017a208bc90c79b7507913a0104849ee9b
Ian Moffett <ian@osmora.org>
2025-10-27 23:49:53 -0400

    ntos: hal: Add IRQL management layer
    The IRQL management layer handles the setting and acquisition of the
    current Interrupt Request Level for the current processor
    
    Signed-off-by: Ian Moffett <ian@osmora.org>

---
+++ ntos/include/hal/irql.h
@@ -0,0 +1,36 @@
+/*
+ * ALGAE interrupt request level management
+ * Copyright (c) 2025, Ian Moffett and the Algae team.
+ * All rights reserved.
+ * Provided under the BSD 3-Clause license.
+ */
+
+#ifndef _HAL_IPI_H_
+#define _HAL_IPI_H_ 1
+
+#include <ke/types.h>
+#include <ke/kpcr.h>
+
+/* Interrupt request levels (IRQLs) */
+#define PASSIVE_LEVEL   0x1
+#define DISPATCH_LEVEL  0x2
+#define CLOCK_LEVEL     0x3
+#define IPI_LEVEL       0x5
+#define POWER_LEVEL     0x6
+#define HIGH_LEVEL      0x7
+
+/*
+ * Get the current interrupt request level
+ *
+ * @core: Processor core to get the IRQL from
+ */
+ULONG halGetIrql(void);
+
+/*
+ *
+ * @core: Processor core to set the IRQL for
+ * @irql: IRQL to set
+ */
+void halSetIrql(ULONG irql);
+
+#endif  /* !_HAL_IPI_H_ */
---
+++ ntos/platform/amd64/cpu/irql.c
@@ -0,0 +1,64 @@
+/*
+ * ALGAE interrupt request level management
+ * Copyright (c) 2025, Ian Moffett and the Algae team.
+ * All rights reserved.
+ * Provided under the BSD 3-Clause license.
+ */
+
+#include <hal/irql.h>
+#include <ke/assert.h>
+#include <ke/defs.h>
+#include <ke/kpcr.h>
+
+/*
+ * Reads the contents of the task priority register
+ * which is windowed (no pun intended) by the CR8
+ * register
+ */
+ALWAYS_INLINE static inline ULONG
+readCr8(void)
+{
+    UQUAD cr8;
+
+    ASMV(
+        "mov %%cr8, %0"
+        : "=r" (cr8)
+        :
+        : "memory"
+    );
+
+    return cr8;
+}
+
+ALWAYS_INLINE static inline void
+writeCr8(ULONG value)
+{
+    UQUAD value64 = (UQUAD)value;
+
+    ASMV(
+        "mov %0, %%cr8"
+        :
+        : "r" (value64)
+        : "memory"
+    );
+}
+
+ULONG
+halGetIrql(void)
+{
+    ULONG cr8;
+
+    cr8 = readCr8();
+    return (cr8 >> 4) & 0xF;
+}
+
+void
+halSetIrql(ULONG irql)
+{
+    ULONG cr8;
+
+    cr8 = readCr8();
+    cr8 &= ~(0xF << 4);
+    cr8 |= irql;
+    writeCr8(cr8);
+}
