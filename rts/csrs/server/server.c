e6f8053deb1bfc08114514530a854efd1d6453c7
Ian Moffett <ian@osmora.org>
2025-10-30 13:36:02 -0400

    project: Add initial userland stubs
    Signed-off-by: Ian Moffett <ian@osmora.org>

---
+++ rts/csrs/server/server.c
@@ -0,0 +1,16 @@
+/*
+ * ALGAE Client Server Runtime Service server
+ * Copyright (c) 2025, Ian Moffett and the Algae team.
+ * All rights reserved.
+ * Provided under the BSD 3-Clause license.
+ */
+
+#include <ntstatus.h>
+#include <types.h>
+
+int
+_start(void)
+{
+    for (;;);
+    return STATUS_SUCCESS;
+}
---
+++ sdk/include/types.h
@@ -0,0 +1,13 @@
+/*
+ * ALGAE SDK user side types
+ * Copyright (c) 2025, Ian Moffett and the Algae team.
+ * All rights reserved.
+ * Provided under the BSD 3-Clause license.
+ */
+
+#ifndef _SDK_TYPES_H_
+#define _SDK_TYPES_H_ 1
+
+#include <ke/types.h>
+
+#endif  /* !_SDK_TYPES_H_ */
